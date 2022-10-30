dc54c4f44161f22feb569e028065d93a93cdc2a8
NicoleBorbe <nicole.borbe@gmail.com>
2022-10-30 20:04:19 +0100

    version1

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+  ];
+ 
+let text = document.getElementById("floatingTextarea");
+let headline = document.getElementById("headline");
+let explanation = document.getElementById("explanation");
+let code = document.getElementById("code");
+let letters = "";
+let check;
+
+function encrypt() {
+	for (letters of text.value.toUpperCase()) {
+    // console.log(letters);
+		for (check of morseAlphabet) {
+			if (check.letter == letters) {
+        headline.innerHTML = "Explanation:" + "<br>";
+        // console.log(check.letter);
+        // console.log(check.morseCode);
+        explanation.innerHTML += check.letter + "= " + check.morseCode + "<br>";
+        code.innerHTML += check.morseCode;
+		  } 
+	  }
+  }
+};
